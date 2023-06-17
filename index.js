4d813fa49eec9a333fb1315bdbac9ce14601e5e5
Ahmed <ahmed.sabry@outlook.com>
2023-06-17 15:01:39 -0400

    copied from codepen

--- index.js
+++ index.js
@@ -1,6 +1,118 @@
-// Import stylesheets
-import './style.css';
+const msg1 = { head: 'head1', msg: 'Hello from msg1', Yes: 'A1', No: 'B1', Ok: 'ok'}
+const msg2 = { head: 'head1', msg: 'Hello from msg2', Yes: 'A2', No: 'B2', Ok: 'ok'}
+//----------------------------------------------------------
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const $myModal = document.getElementById('modal')
+
+if ($myModal) {  
+	$myModal.addEventListener('show.bs.modal', event => {
+	// console.log('@myModal')
+		
+		// Button that triggered the modal
+		const button = event.relatedTarget
+		console.log('button => ', button)
+
+		// Extract info from data-bs-* attributes
+		const recipient = button.getAttribute('data-bs-whatever')
+		console.log('recipient => ', recipient)
+
+		// If necessary, you could initiate an Ajax request here
+		// and then do the updating in a callback.
+
+		// Update the modal's content.
+		const modalTitle = $myModal.querySelector('.modal-title')
+		// const modalBodyInput = $myModal.querySelector('.modal-body input')
+
+		modalTitle.textContent = `New message to ${recipient}`
+		modalBodyInput.value = recipient
+	})
+}
+//----------------------------------------------------------
+
+//----------------------------------------------------------
+let $main = document.getElementById('main')
+let $modal
+// const showModal = (title, description, yesBtnLabel = 'Yes', noBtnLabel = 'Cancel', callback) => {
+const showModal = (flag, obj) => {
+	if(!$modal) {
+		$modal = document.createElement('div')
+		$modal.id = 'modal'
+		$modal.setAttribute('data-bs-keyboard', 'true')
+		$modal.setAttribute('tabindex', '-1')
+		$modal.setAttribute('aria-labelledby', 'modalLabel')
+		$modal.setAttribute('aria-hidden', 'true')
+			// $main.append($modal);
+	} else {
+		// $modal.parentNode.removeChild($modal)
+		$modal.innerHTML = ''
+		$modal.hide()
+		$modal.dispose()
+		// console.log($main)
+		
+		// $modal = document.createElement('div')
+		// $modal.id = 'modal'
+		// $modal.setAttribute('data-bs-keyboard', 'true')
+		// $modal.setAttribute('tabindex', '-1')
+		// $modal.setAttribute('aria-labelledby', 'modalLabel')
+		// $modal.setAttribute('aria-hidden', 'true')
+	}
+	// console.log($main)
+	
+	$modal.innerHTML = `
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header justify-content-center border-0">
+						<h5 class="modal-title fs-5" id="modalLabel">${obj.head} : ${flag}</h5>
+						${ flag === 0 ? `<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>` : ''}
+					</div>
+					<div class="modal-body text-center">
+						<p>${obj.msg}</p>
+					</div>
+					<div class="modal-footer justify-content-center border-0">
+					${ flag === 1 ? `<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">${!obj.Ok ? `${obj.Yes}` : `${obj.Ok}`}</button>` : ''}
+					
+					${ flag === 2 ? `<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">${obj.Yes}</button>` : '' }
+					${ flag === 2 ? `<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">${obj.No}</button>` : '' }
+					</div>					
+				</div>
+			</div>
+	`;
+	console.log($modal.innerHTML)
+	// $modal = bootstrap.Modal.getOrCreateInstance($modal)
+	$modal = new bootstrap.Modal($modal)
+	$modal.show();
+
+	$main.append($modal);
+	console.log($main)
+}
+//----------------------------------------------------------
+
+const populate_msg = function(e) {
+	/*
+	First parameter: [
+		0: close btn only,
+		1: One Ok button (creates the first button only),
+		2: Two buttons (Yes, No)
+	]
+	*/
+	e.target.id === 'en' ? 	showModal(2, msg1) : ''
+	e.target.id === 'fr' ? 	showModal(2, msg2) : ''
+	
+	e.target.id === 'info' ? 	showModal(1, msg1) : ''
+	e.target.id === 'close' ? 	showModal(0, msg2) : ''
+}
+
+const $en = document.querySelector('#en')
+const $fr = document.querySelector('#fr')
+const $info = document.querySelector('#info')
+const $close = document.querySelector('#close')
+
+$en.addEventListener('click', populate_msg)
+$fr.addEventListener('click', populate_msg)
+$info.addEventListener('click', populate_msg)
+$close.addEventListener('click', populate_msg)
+
+// $en.addEventListener('click', showModal(2, msg1))
+// $fr.addEventListener('click', showModal(2, msg2))
+// $info.addEventListener('click', showModal(1, msg1))
+// $close.addEventListener('click', showModal(0, msg2))
