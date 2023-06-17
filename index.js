20d5df15cd75f60c1a7125c49bef4857234aa463
Ahmed <ahmed.sabry@outlook.com>
2023-06-17 16:02:38 -0400

    other way

--- index.js
+++ index.js
@@ -2,19 +2,65 @@ const msg1 = { head: 'head1', msg: 'Hello from msg1', Yes: 'A1', No: 'B1', Ok: '
 const msg2 = { head: 'head1', msg: 'Hello from msg2', Yes: 'A2', No: 'B2', Ok: 'ok'}
 //----------------------------------------------------------
 
-const $myModal = document.getElementById('modal')
+const populate_msg = (e) => {
+	console.log(e)
+}
 
-if ($myModal) {  
+const $en = document.querySelector('#en')
+const $fr = document.querySelector('#fr')
+const $info = document.querySelector('#info')
+const $close = document.querySelector('#close')
+
+$en.addEventListener('click', populate_msg)
+$fr.addEventListener('click', populate_msg)
+$info.addEventListener('click', populate_msg)
+$close.addEventListener('click', populate_msg)
+
+
+const $myModal = document.getElementById('modal')
+if ($myModal) {
 	$myModal.addEventListener('show.bs.modal', event => {
-	// console.log('@myModal')
-		
 		// Button that triggered the modal
-		const button = event.relatedTarget
-		console.log('button => ', button)
+		const btnTarget = event.relatedTarget
+		console.log('btnTarget => ', btnTarget)
 
 		// Extract info from data-bs-* attributes
-		const recipient = button.getAttribute('data-bs-whatever')
-		console.log('recipient => ', recipient)
+		const dataObj = btnTarget.getAttribute('data-bs-obj')
+		console.log('dataObj => ', dataObj)
+
+		const dataCat = btnTarget.getAttribute('data-bs-cat')
+		console.log('dataCat => ', dataCat)
+
+		const dataMsg = btnTarget.getAttribute('data-bs-msg')
+		console.log('dataMsg => ', dataMsg)
+
+		// If necessary, you could initiate an Ajax request here
+		// and then do the updating in a callback.
+
+		// Update the modal's content.
+		const modalTitle = $myModal.querySelector('.modal-title')
+		const modalBody = $myModal.querySelector('.modal-body')
+		// const modalBodyInput = $myModal.querySelector('.modal-body input')
+
+		modalTitle.textContent = `Data: ${btnTarget} | ${dataObj} | ${dataCat} | ${dataMsg}`
+		modalBody.textContent = `Data: ${btnTarget} | ${dataObj} | ${dataCat} | ${dataMsg}`
+		// modalBodyInput.value = dataTarget
+	})
+}
+
+
+/*
+const $myModal = document.getElementById('modal')
+
+if ($myModal) {
+	$myModal.addEventListener('show.bs.modal', event => {
+		// Button that triggered the modal
+		const btnTarget = event.relatedTarget
+		console.log('btnTarget => ', btnTarget)
+
+		// Extract info from data-bs-* attributes
+		const dataTarget = button.getAttribute('data-bs-whatever')
+		console.log('dataTarget => ', dataTarget)
 
 		// If necessary, you could initiate an Ajax request here
 		// and then do the updating in a callback.
@@ -23,8 +69,8 @@ if ($myModal) {
 		const modalTitle = $myModal.querySelector('.modal-title')
 		// const modalBodyInput = $myModal.querySelector('.modal-body input')
 
-		modalTitle.textContent = `New message to ${recipient}`
-		modalBodyInput.value = recipient
+		modalTitle.textContent = `New message to ${dataTarget}`
+		modalBodyInput.value = dataTarget
 	})
 }
 //----------------------------------------------------------
@@ -46,7 +92,7 @@ const showModal = (flag, obj) => {
 		// $modal.parentNode.removeChild($modal)
 		$modal.innerHTML = ''
 		$modal.hide()
-		$modal.dispose()
+		// $modal.dispose()
 		// console.log($main)
 		
 		// $modal = document.createElement('div')
@@ -77,24 +123,24 @@ const showModal = (flag, obj) => {
 				</div>
 			</div>
 	`;
-	console.log($modal.innerHTML)
+	// console.log($modal.innerHTML)
 	// $modal = bootstrap.Modal.getOrCreateInstance($modal)
-	$modal = new bootstrap.Modal($modal)
-	$modal.show();
+	let modal = new bootstrap.Modal($modal)
+	modal.show();
 
-	$main.append($modal);
+	$main.append(modal);
 	console.log($main)
 }
 //----------------------------------------------------------
 
 const populate_msg = function(e) {
-	/*
-	First parameter: [
-		0: close btn only,
-		1: One Ok button (creates the first button only),
-		2: Two buttons (Yes, No)
-	]
-	*/
+
+	// First parameter: [
+		// 0: close btn only,
+		// 1: One Ok button (creates the first button only),
+		// 2: Two buttons (Yes, No)
+	// ]
+
 	e.target.id === 'en' ? 	showModal(2, msg1) : ''
 	e.target.id === 'fr' ? 	showModal(2, msg2) : ''
 	
@@ -116,3 +162,5 @@ $close.addEventListener('click', populate_msg)
 // $fr.addEventListener('click', showModal(2, msg2))
 // $info.addEventListener('click', showModal(1, msg1))
 // $close.addEventListener('click', showModal(0, msg2))
+
+*/
